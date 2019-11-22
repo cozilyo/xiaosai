76e4aa3020cbbee703530ce3150b8ece5c43252e
pen <los0412@163.com>
2019-11-22 17:40:32 +0800

    添加工具类

---
+++ src/test/java/com/cozi/xiaosai/util/MailSendUtilsTests.java
@@ -0,0 +1,19 @@
+package com.cozi.xiaosai.util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author xiaosai
+ * @Date 2019-11-21 21:27
+ * @Version 1.0
+ */
+
+@SpringBootTest
+public class MailSendUtilsTests {
+
+    @Test
+    public void test(){
+        System.out.println("111");
+    }
+}
