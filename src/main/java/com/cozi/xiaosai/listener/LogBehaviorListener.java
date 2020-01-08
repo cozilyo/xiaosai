2940cd93e300ab1db1f0741a1b164da022eb1254
冯志龙 <cozilyo@163.com>
2020-01-08 14:16:36 +0800

    日志操作对象修改

--- src/main/java/com/cozi/xiaosai/listener/LogBehaviorListener.java
+++ src/main/java/com/cozi/xiaosai/listener/LogBehaviorListener.java
@@ -2,6 +2,7 @@ package com.cozi.xiaosai.listener;
 
 import com.cozi.xiaosai.domain.LogInfo;
 import com.cozi.xiaosai.event.LogBehaviorEvent;
+import com.cozi.xiaosai.pojo.dataorigin.sys.User;
 import com.cozi.xiaosai.service.tool.Log1oneService;
 import com.cozi.xiaosai.util.IpUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * 日志自定义事件监听
@@ -35,11 +37,19 @@ public class LogBehaviorListener {
             //从获取RequestAttributes中获取HttpServletRequest的信息
             HttpServletRequest request = (HttpServletRequest) requestAttributes.resolveReference(RequestAttributes.REFERENCE_REQUEST);
 
-            String ipAddr = IpUtil.getIpAddr(request);
+            //获取session
+            HttpSession session = request.getSession();
+            User user = (User)session.getAttribute("user");
 
+            //获取ip地址
+            String ipAddr = IpUtil.getIpAddr(request);
             logInfo.setIp(ipAddr);
 
-            logInfo.setOperator("admin");
+            if(user!=null){
+                logInfo.setOperator(user.getUserName());
+            }else {
+                logInfo.setOperator("");
+            }
 
             log1oneService.addLog(logInfo);
         }
